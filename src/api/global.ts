ae982fc93687272b9c1ba7b355e48803e66c7b40
Ailzr <ailzr@outlook.com>
2025-05-02 19:16:39 +0800

    Update global.ts

--- src/api/global.ts
+++ src/api/global.ts
@@ -1,6 +1,6 @@
 export const global = {
     path: import.meta.env.VITE_API_HOST + "/apis" || `${window.location.protocol}//${window.location.hostname}:80/apis`,
-    ws_path: import.meta.env.VITE_WS_HOST + "/ws" || `${window.location.protocol === 'https:' ? 'wss:' : 'ws:'}//${window.location.hostname}:80/ws`,
+    ws_path: import.meta.env.VITE_WS_HOST + "/apis/ws" || `${window.location.protocol === 'https:' ? 'wss:' : 'ws:'}//${window.location.hostname}:80/apis/ws`,
     token: localStorage.getItem('video-web-golang-token') || '',
     socket: null as WebSocket | null,
     currentChatUser: null as string | null
